8f08b4287c6d9bd0a11fde0b3fc6e7b578d48bb7
unknown <amirmp012@gmail.com>
2021-10-23 14:18:02 +0330

    safe project first push

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    $('.humberger').click(function(){
+        $(this).toggleClass('humberger-change');
+        $(".menu").toggleClass("left-0");
+        $('.opened-menu').toggleClass('transform-0');
+    });
+
+    $('#mobile_header li#services').click(function(){
+        $('#mobile_header li#services > ul.inner-ul').slideToggle(400);
+        
+    });
+    
+    new WOW().init();
+
+    setInterval(() => {
+        if ($('#carousel_item_1').hasClass('active')) {
+            $('.slider-titles').removeClass('title-active');
+            $('#slider_title_1').addClass('title-active');
+        }
+        if ($('#carousel_item_2').hasClass('active')) {
+            $('.slider-titles').removeClass('title-active');
+            $('#slider_title_2').addClass('title-active');
+        }
+        if ($('#carousel_item_3').hasClass('active')) {
+            $('.slider-titles').removeClass('title-active');
+            $('#slider_title_3').addClass('title-active');
+        }
+        if ($('#carousel_item_4').hasClass('active')) {
+            $('.slider-titles').removeClass('title-active');
+            $('#slider_title_4').addClass('title-active');
+        }
+    }, 1);
+
+
+    // clicking on slider titles
+    $('.choose-slider .slider-titles').click(function(){
+        $('.slider-titles').removeClass('title-active');
+        $(this).addClass('title-active');
+    });
+
+    $('#slider_title_1').click(function(){
+        $('.carousel-item').removeClass('active');
+        $('#carousel_item_1').addClass('active');
+    });
+    $('#slider_title_2').click(function(){
+        $('.carousel-item').removeClass('active');
+        $('#carousel_item_2').addClass('active');
+    });
+    $('#slider_title_3').click(function(){
+        $('.carousel-item').removeClass('active');
+        $('#carousel_item_3').addClass('active');
+    });
+    $('#slider_title_4').click(function(){
+        $('.carousel-item').removeClass('active');
+        $('#carousel_item_4').addClass('active');
+    });
+
+});
+
