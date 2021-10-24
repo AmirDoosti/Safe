3609e5633bcdb536c7d3e62cba40c2c1d82396bb
unknown <amirmp012@gmail.com>
2021-10-24 23:33:04 +0330

    safe ui completed

--- js/main.js
+++ js/main.js
@@ -57,5 +57,24 @@ $(document).ready(function () {
         $('#carousel_item_4').addClass('active');
     });
 
+
+    $('#works_slider').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+
 });
 
